ab4884fa323da17098502e5301a748e91f8b91e0
Ilya Chichak <ilyachch@gmail.com>
2019-01-12 03:05:57 +0700

    made tests

---
+++ tests/test_filesworker.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+from md_translate.exceptions import DirectoryNotFoundException
+from md_translate.files_worker import FilesWorker
+
+
+class TestFilesWorker(unittest.TestCase):
+    test_data_folder = os.path.join('data', 'test_data')
+
+    def test_folder_errors(self):
+        with self.assertRaises(DirectoryNotFoundException):
+            FilesWorker('not existing folder')
+        with self.assertRaises(NotADirectoryError):
+            FilesWorker(os.path.join(self.test_data_folder, 'not_a_folder'))
+        with self.assertRaises(FileNotFoundError):
+            FilesWorker(os.path.join(self.test_data_folder, 'empty_folder'))
+        with self.assertRaises(FileNotFoundError):
+            FilesWorker(os.path.join(self.test_data_folder, 'folder_without_md_files'))
+        self.assertListEqual(
+            ['first_file.md', 'second_file.md'],
+            FilesWorker(os.path.join(self.test_data_folder, 'md_files_folder')).md_files_list
+        )
---
+++ tests/test_translator.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+from unittest import mock
+
+from md_translate.translator import Translator
+
+
+class TestTranslatorException(unittest.TestCase):
+    def setUp(self):
+        Translator.API_KEY = None
+
+    def test_api_key_exception(self):
+        with self.assertRaises(FileNotFoundError):
+            Translator()
+
+
+class TestTranslator(unittest.TestCase):
+    def setUp(self):
+        self.api_key = 'TEST_API_KEY'
+        dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        self.api_key_path = os.path.join(dir_path, 'data', 'test_data', 'API_KEY')
+        self.api_key_backup = Translator.api_key_filename
+        Translator.api_key_filename = self.api_key_path
+
+    def test_api_key_caching(self):
+        translator1 = Translator()
+        self.assertEqual(Translator.API_KEY, self.api_key)
+        translator2 = Translator()
+        self.assertNotEqual(translator1, translator2)
+        self.assertEqual(translator1.api_key, translator2.api_key)
+
+    @mock.patch('md_translate.translator.requests.post')
+    def test_translation_requesting(self, mocked_post):
+        string_to_translate = 'Some string to translate'
+        translated_string = 'Some string to translate'
+
+        class ResponseMock:
+            def json(self):
+                return {'data': [translated_string]}
+
+        mocked_post.return_value = ResponseMock()
+        translate_result = Translator().request_translation(string_to_translate)
+        mocked_post.assert_called_with(
+            'https://translate.yandex.net/api/v1.5/tr.json/translate?key=TEST_API_KEY&lang=en-ru',
+            {'text': string_to_translate}
+        )
+        self.assertEqual(translate_result, translated_string)
+
+    def tearDown(self):
+        Translator.api_key_filename = self.api_key_backup
+        Translator.API_KEY = None
