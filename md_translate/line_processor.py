5046cd8e9fc10eb19fb57e6575f0617670b56fb1
Ilya Chichak <ilyachch@gmail.com>
2019-05-12 00:34:08 +0700

    V0.1.1 (#9)
    * fixed translator class instancing
    
    * updated bumpversion config
    
    * Bump version: 0.1.0 → 0.1.1
    
    * fixed tests

--- md_translate/line_processor.py
+++ md_translate/line_processor.py
@@ -1,12 +1,13 @@
 import re
+from md_translate.arguments_processor import settings
 
 
 class LineProcessor:
     code_mark: str = '```'
-    paragraph_regexp = re.compile(r'^[a-zA-Z]+.*')
 
     def __init__(self, line: str):
         self._line: str = line
+        self.pattern = self.get_regexp(settings.source_lang)
 
     def is_code_block_border(self):
         if self._line == self.code_mark:
@@ -17,7 +18,16 @@ class LineProcessor:
         return not self.__is_single_code_line() and self.__is_untranslated_paragraph()
 
     def __is_untranslated_paragraph(self):
-        return self.paragraph_regexp.match(self._line)
+        return re.match(self.pattern, self._line) is not None
 
     def __is_single_code_line(self):
         return self._line.startswith(self.code_mark) and self._line.endswith(self.code_mark) and len(self._line) > 3
+
+    @staticmethod
+    def get_regexp(source_lang):
+        if source_lang == 'ru':
+            return r'^[а-яА-Я]+.*'
+        elif source_lang == 'en':
+            return r'^[a-zA-Z]+.*'
+        else:
+            return r'^\d+.*'
