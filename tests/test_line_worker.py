e9febaa377288d545695f951b68854e8e28caa47
Ilya Chichak <ichichak@sinergo.ru>
2019-05-13 14:12:17 +0700

    fixed tests

--- tests/test_line_worker.py
+++ tests/test_line_worker.py
@@ -1,14 +1,6 @@
 import unittest
-from unittest import mock
 
 from md_translate.line_processor import LineProcessor
-
-
-class MockedSettings:
-    api_key = 'TEST_API_KEY'
-    source_lang = 'en'
-    target_lang = 'ru'
-    service = 'Yandex'
 
 
 class TestLineProcessorWithCode(unittest.TestCase):
@@ -23,21 +15,28 @@ class TestLineProcessorWithCode(unittest.TestCase):
     QUOTE_BLOCK_STR = '> Quote'
     LIST_ITEM_STR = '* List Item'
 
-    @mock.patch('md_translate.line_processor.settings', new_callable=MockedSettings)
-    def test_code_block_border(self, mocked_settings):
-        self.assertTrue(LineProcessor(self.CODE_BLOCK_STR).is_code_block_border())
-        self.assertTrue(LineProcessor(self.PYTHON_CODE_BLOCK_STR).is_code_block_border())
-        self.assertTrue(LineProcessor(self.BASH_CODE_BLOCK_STR).is_code_block_border())
-        self.assertFalse(LineProcessor(self.SINGLE_LINE_CODE_BLOCK_STR).is_code_block_border())
-        self.assertFalse(LineProcessor(self.NOT_CODE_BLOCK_STR).is_code_block_border())
+    class MockedSettings:
+        api_key = 'TEST_API_KEY'
+        source_lang = 'en'
+        target_lang = 'ru'
+        service = 'Yandex'
 
-    @mock.patch('md_translate.line_processor.settings', new_callable=MockedSettings)
-    def test_line_can_be_translated(self, mocked_settings):
-        self.assertTrue(LineProcessor(self.PARAGRAPH_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.CODE_BLOCK_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.PYTHON_CODE_BLOCK_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.BASH_CODE_BLOCK_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.SINGLE_LINE_CODE_BLOCK_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.TRANSLATED_PARAGRAPH_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.QUOTE_BLOCK_STR).line_can_be_translated())
-        self.assertFalse(LineProcessor(self.LIST_ITEM_STR).line_can_be_translated())
+    def setUp(self) -> None:
+        self.settings = self.MockedSettings()
+
+    def test_code_block_border(self):
+        self.assertTrue(LineProcessor(self.settings, self.CODE_BLOCK_STR).is_code_block_border())
+        self.assertTrue(LineProcessor(self.settings, self.PYTHON_CODE_BLOCK_STR).is_code_block_border())
+        self.assertTrue(LineProcessor(self.settings, self.BASH_CODE_BLOCK_STR).is_code_block_border())
+        self.assertFalse(LineProcessor(self.settings, self.SINGLE_LINE_CODE_BLOCK_STR).is_code_block_border())
+        self.assertFalse(LineProcessor(self.settings, self.NOT_CODE_BLOCK_STR).is_code_block_border())
+
+    def test_line_can_be_translated(self):
+        self.assertTrue(LineProcessor(self.settings, self.PARAGRAPH_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.CODE_BLOCK_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.PYTHON_CODE_BLOCK_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.BASH_CODE_BLOCK_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.SINGLE_LINE_CODE_BLOCK_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.TRANSLATED_PARAGRAPH_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.QUOTE_BLOCK_STR).line_can_be_translated())
+        self.assertFalse(LineProcessor(self.settings, self.LIST_ITEM_STR).line_can_be_translated())
--- tests/test_translator.py
+++ tests/test_translator.py
@@ -4,34 +4,33 @@ from unittest import mock
 from md_translate.translator import YandexTranslator
 
 
-class MockedSettings:
-    api_key = 'TEST_API_KEY'
-    source_lang = 'en'
-    target_lang = 'ru'
-    service = 'Yandex'
-
-
-class MockedResponse:
-    translated_string = 'Переведенная строка'
-    ok = True
-
-    def json(self):
-        return {'text': [self.translated_string, ]}
 
 
 class TestTranslator(unittest.TestCase):
+    class MockedSettings:
+        api_key = 'TEST_API_KEY'
+        source_lang = 'en'
+        target_lang = 'ru'
+        service = 'Yandex'
+
+    class MockedResponse:
+        translated_string = 'Переведенная строка'
+        ok = True
+
+        def json(self):
+            return {'text': [self.translated_string, ]}
+
     def setUp(self):
         self.string_to_translate = 'Some string to translate'
         self.translated_string = 'Переведенная строка'
 
-        self.mocked_settings = MockedSettings()
-        self.mocked_response = MockedResponse()
+        self.mocked_settings = self.MockedSettings()
+        self.mocked_response = self.MockedResponse()
 
-    @mock.patch('md_translate.translator.settings', new_callable=MockedSettings)
     @mock.patch('md_translate.translator.requests.post')
-    def test_translation_requesting(self, mocked_post, mocked_settings):
+    def test_translation_requesting(self, mocked_post):
         mocked_post.return_value = self.mocked_response
-        translate_result = YandexTranslator().request_translation(self.string_to_translate)
+        translate_result = YandexTranslator(self.mocked_settings).request_translation(self.string_to_translate)
         mocked_post.assert_called_with(
             'https://translate.yandex.net/api/v1.5/tr.json/translate',
             data={'text': 'Some string to translate'}, params={'key': 'TEST_API_KEY', 'lang': 'en-ru'}
